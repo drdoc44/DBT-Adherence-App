e8c42bb2f721e04882190a83b01edd20525f648d
drdoc44 <72724211+drdoc44@users.noreply.github.com>
2020-10-18 22:17:31 -0700

    Add files via upload

---
+++ navAppend.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+  $(".navbar .container-fluid .navbar-nav").append('<li><a href="https://login.panopto.com/?_ga=2.231228637.390442522.1549577689-2042957927.1549577689" target="_blank">Video</a></li>');
+});
